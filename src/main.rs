a276a4fbd574e41526798ad8fb168798bfef209d
aujxn <ajn6@pdx.edu>
2020-06-25 11:06:22 -0700

    changed pins

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use rppal::gpio::{Gpio, OutputPin};
 use std::error::Error;
-use std::io;
 use std::time::{Duration, Instant};
 use time::{OffsetDateTime, Time};
 
 #[macro_use]
 extern crate time;
 
-const GPIO_LIGHTS: u8 = 4;
+const GPIO_LIGHTS: u8 = 27;
 const LIGHTS_ON_TIME: Time = time!(7:00);
 const LIGHTS_OFF_TIME: Time = time!(21:00);
 
-const GPIO_MIST: u8 = 17;
+const GPIO_MIST: u8 = 22;
 const MIST_ON_TIME: Time = time!(19:00);
 const MIST_OFF_TIME: Time = time!(19:01);
 
-const GPIO_FOG: u8 = 18;
-const FOG_ON_INTERVAL: Duration = Duration::from_secs(60 * 30);
+const GPIO_FOG: u8 = 17;
+const FOG_ON_INTERVAL: Duration = Duration::from_secs(60 * 10);
 const FOG_OFF_INTERVAL: Duration = Duration::from_secs(60 * 30);
 
 struct Lights {
@@ -53,6 +52,7 @@ impl Mist {
 
     fn update(&mut self) {
         let now = OffsetDateTime::now_local().time();
+        // set low for the relay means on
         if now > MIST_ON_TIME && now < MIST_OFF_TIME {
             self.pin.set_low();
         } else {
@@ -79,10 +79,10 @@ impl Fog {
         let elapsed = Instant::now().duration_since(self.last_switch);
 
         // set low for the relay means on
-        if self.pin.is_set_low() && elapsed > FOG_ON_INTERVAL {
+        if self.pin.is_set_low() && elapsed > FOG_OFF_INTERVAL {
             self.pin.set_high();
             self.last_switch = Instant::now();
-        } else if self.pin.is_set_high() && elapsed > FOG_OFF_INTERVAL {
+        } else if self.pin.is_set_high() && elapsed > FOG_ON_INTERVAL {
             self.pin.set_low();
             self.last_switch = Instant::now();
         }
